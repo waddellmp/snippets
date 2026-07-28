c2ab0dca5020d6369160a82b5407c590d3115ed1
Matthew Waddell <waddellmp08@gmail.com>
2026-07-28 08:41:16 -0500

    docs: clarify GENERATE_SERIES operation in SQL table creation script

--- postgresql/sql/create_table.sql
+++ postgresql/sql/create_table.sql
@@ -1,4 +1,4 @@
 CREATE TABLE tbl (col SMALLINT);
 
--- insert some data using GENERATE_SERIES function
+-- insert some data using GENERATE_SERIES function by inserting a series of values (1-10) into tbl.col
 INSERT INTO tbl(col) SELECT GENERATE_SERIES(1, 10);
